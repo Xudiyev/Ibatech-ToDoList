52c5cbcc7b6f1969c00d1d5c178d16f6fa4d4d48
Arkan <arkankhudiyev@gmail.com>
2023-03-30 02:06:35 +0400

    JS-task

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let inpt = document.getElementById('inpt')
+let btn = document.getElementById('btn')
+let ul = document.getElementById('ul')
+let todo = [];
+
+function add() {
+
+    todo.push(inpt.value);
+    inpt.value = '';
+
+
+    let data = '';
+    for (let item of todo) {
+
+        if (item == '') {
+            data == ''
+
+        } else {
+            data += `<li class="list-group-item">${item}</li>`
+            
+        }
+       
+        
+    }
+   
+
+    ulist.innerHTML = data;
+   
+}
+btn.addEventListener('click', add);
