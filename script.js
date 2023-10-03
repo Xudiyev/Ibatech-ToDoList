c3cc62b2aae91ea69383836cff47dc10c161e22f
Arkan <arkankhudiyev@gmail.com>
2023-10-03 17:24:54 +0400

    To Do task

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 
-let inpt = document.getElementById('inpt')
-let btn = document.getElementById('btn')
-let ul = document.getElementById('ul')
-let todo = [];
+let input = document.getElementById('inpt')
+let button = document.getElementById('btn')
+let ulist = document.getElementById('ulist')
+button.addEventListener('click', () => {
+    if (input.value == 0) {
+        alert("Kindly Enter Task Name!!!!")
 
-function add() {
-
-    todo.push(inpt.value);
-    inpt.value = '';
-
-
-    let data = '';
-    for (let item of todo) {
-
-        if (item == '') {
-            data == ''
-
-        } else {
-            data += `<li class="list-group-item">${item}</li>`
-            
-        }
-       
-        
     }
-   
+    else {
+        ulist.innerHTML += `
+        <li class="list-group-item">${input.value} 
+        <button  class="btn btn-danger " id = "deleteBtn">Delete</button>
+        <button id="doneBtn" class="btn btn-info">Done</button>
+        </li>
+        `;
+        input.value = '';
+        var mylists = document.querySelectorAll("#deleteBtn");
+        // We take button length
+        for(var i=0; i<mylists.length; i++){
+            // We choose whic list we a want to delete
+            mylists[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        // Done button 
+        var doneButton=document.querySelectorAll("#doneBtn")
+        for(var i=0; i<doneButton.length; i++){
+            doneButton[i].onclick = function(){
+                this.parentNode.style.textDecoration="line-through";
+            }
+        }
+    }
+});
 
-    ulist.innerHTML = data;
-   
-}
-btn.addEventListener('click', add);
